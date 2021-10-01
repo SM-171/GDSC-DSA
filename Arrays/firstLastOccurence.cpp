e85ea56ae03eda4a32f3a5b1abe64db401118328
Shreya Mittal <shreyamittal117@gmail.com>
2021-10-01 22:20:57 +0530

    commit on main

---
+++ Arrays/firstLastOccurence.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.size() == 0) {
+            return {-1, -1};
+        }
+        
+        int firstOccurence = BinarySearch(nums, target);
+        int lastOccurence = BinarySearch(nums, target + 1) - 1;
+        if (firstOccurence < nums.size() && nums[firstOccurence] == target)
+            return {firstOccurence, lastOccurence};
+        else
+            return {-1, -1};
+    }
+
+    
+    int BinarySearch(vector<int>& nums, int target) 
+    {
+        int low = 0;
+        int high = nums.size()-1;
+        
+        while (low <= high) 
+        {
+            int mid = low + (high - low)/2;
+            if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+    }
+};
--- Arrays/printReverse.cpp
+++ Arrays/printReverse.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -25,31 +24,3 @@ int main() {
 	}
 	return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-
-void printReverse(int* arr, int size) {
-    for(int i = size - 1; i >= 0; i--) {
-        cout << *(arr + i) << " ";
-    }
-}
-
-int main() {
-	int testCases;
-	cin >> testCases;
-	
-	while(testCases--) {
-	    int size;
-	    cin >> size;
-	
-	    int arr[size];
-	    for(int i = 0; i < size; i++) {
-	        cin >> arr[i];
-	    }
-
-	    printReverse(arr, size);
-	}
-	return 0;
-}
->>>>>>> 851ab44f1df07037ed245cc6101d6060bc0628a2
