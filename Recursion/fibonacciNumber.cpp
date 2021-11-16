4ff55ecdad7d44504e11284668a11d8030972d68
Shreya Mittal <79536730+SM-171@users.noreply.github.com>
2021-11-16 16:35:50 +0530

    Update fibonacciNumber.cpp

--- Recursion/fibonacciNumber.cpp
+++ Recursion/fibonacciNumber.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Leetcode question 509. Fibonacci Number
 
 #include <bits/stdc++.h>
@@ -12,18 +11,3 @@ public:
         return fib(n - 1) + fib (n - 2);
     }
 };
-=======
-// Leetcode question 509. Fibonacci Number
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    int fib(int n) {
-        if (n == 0 || n == 1) return n;
-        
-        return fib(n - 1) + fib (n - 2);
-    }
-};
->>>>>>> 8a7dcc9b942a4acb5e23b149ddbcf6e5136377df
