dfbfd5cabb6ecee1593c9fb8051c27719e6bc342
Shreya Mittal <79536730+SM-171@users.noreply.github.com>
2021-11-16 16:37:05 +0530

    Update reverseString.cpp

--- Recursion/reverseString.cpp
+++ Recursion/reverseString.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Leetcode question 344.Reverse String
 
 #include <bits/stdc++.h>
@@ -19,26 +18,4 @@ public:
         s.insert(s.begin(), temp);
         cout << s[0];
     }
-=======
-//Leetcode question 344.Reverse String
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    
-    void reverseString(vector<char>& s) {
-        if(s.size() == 1) {
-            cout << s[0];
-            return;
-        }
-        
-        char temp = s[s.size() - 1];
-        s.pop_back();
-        reverseString(s);
-        s.insert(s.begin(), temp);
-        cout << s[0];
-    }
->>>>>>> 8a7dcc9b942a4acb5e23b149ddbcf6e5136377df
 };
