3405bbf1aa8233c3ada77ef3aa705c8c3ab712bd
Shreya Mittal <79536730+SM-171@users.noreply.github.com>
2021-11-16 16:35:17 +0530

    Update factorial.cpp

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // GFG Question Factorial
 
 #include <bits/stdc++.h>
@@ -10,17 +9,4 @@ long long int factorial(int n){
            return 1;
        }
        return n * factorial(n - 1);
-=======
-// GFG Question Factorial
-
-#include <bits/stdc++.h>
-using namespace std;
-
-long long int factorial(int n){
-       //code here
-       if(n == 1 || n == 0) {
-           return 1;
-       }
-       return n * factorial(n - 1);
->>>>>>> 8a7dcc9b942a4acb5e23b149ddbcf6e5136377df
 }
